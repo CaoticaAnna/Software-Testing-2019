0fb19122430ef3167c67d8e57068f9139c7d138f
Anna Guseva <osdadciaca@gmail.com>
2019-04-07 17:32:41 +0700

    Создан класс Point

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+
+  public double x;
+  public double y;
+
+  public Point (double x, double y){
+   this.x = x;
+   this.x = y;
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public double getY() {
+    return y;
+  }
+}
