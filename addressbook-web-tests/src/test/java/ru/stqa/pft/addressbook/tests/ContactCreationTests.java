a3f0d49ee8ba8e5d6c69eda6d21c1f0d9578e7b2
Anna Guseva <osdadciaca@gmail.com>
2019-05-10 17:15:53 +0700

    Добавлено прикрепление фото в тест создания контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -3,17 +3,21 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase{
 
-  @Test()
+  @Test
   public void testContactCreation() {
     Contacts before = app.contact().all();
+    File photo = new File("src/test/resources/stru.png");
     ContactData contact = new ContactData().withSurName("Guseva").withName("Anna")
             .withHomePhone("+1234").withMobilePhone("+7(342)456").withWorkPhone("8-6-6-6")
-            .withAddress("Moscow, Lenina St. 51, kv. 12");
+            .withAddress("Moscow, Lenina St. 51, kv. 12").withPhoto(photo);
     app.contact().create((contact),true);
     app.goTo().homePage();
     assertThat(app.contact().count(), equalTo(before.size() + 1));
@@ -21,4 +25,6 @@ public class ContactCreationTests extends TestBase{
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
   }
+
+
 }
